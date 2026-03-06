1c6d9e9e5a1ce750a604c80536bcbe1e43bd0f2e
shameera44 <shameerariyas1234@gmail.com>
2026-03-06 06:21:17 +0530

    navbar created .it shows every page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import { Routes, Route } from "react-router-dom"
+import CreatePost from "./pages/CreatePost"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/createPost" element={<CreatePost/>}/>
       </Routes>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-
+import { Link } from "react-router-dom"
+import logo from "../images/logo.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex justify-between items-center bg-orange-600 text-white font-bold px-10 py-2">
+      <div >
+        <img src={logo} alt="logo" className="w-30 h-15" /></div>
+
+      <div className="text-4xl">BlogSphere</div>
+
+      <div className="flex gap-5 text-xl cursor-pointer ">
+        <Link to="/" className="hover:text-black hover:underline">Home</Link>
+        <Link to="/about" className="hover:text-black hover:underline" >About</Link>
+        <Link to="/createPost" className="hover:text-black  hover:underline">CreatePost</Link>
+
+      </div>
+
+
+    </div>
   )
 }
 
